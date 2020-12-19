582128c14db016a389b18dfa22999f4ba301aea6
Konstantin <timokhin_74@hotmail.com>
2020-12-19 13:44:22 +0500

    2nd Commit (Installed node-sass)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Counter from './components/Counter/Counter';
 
+
+
 function App() {
   return (
     <div className="App">
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,4 +1,7 @@
 import React from "react"
+import CounterInput from './CounterInput/CounterInput';
+import CounterButton from './CounterButton/CounterButton';
+import classes from "./Counter.module.scss"
 
 export type CounterPropsType = {
 
@@ -6,7 +9,15 @@ export type CounterPropsType = {
 
 
 const Counter:React.FC<CounterPropsType> = () => {
-    return <div>asd</div>
+    return (
+    <div className={classes.counter}>
+        <CounterInput/>
+        <div className={classes.buttons}>
+            <CounterButton/>
+            <CounterButton/>
+        </div>
+    </div>
+    )
 }
 
 export default Counter
